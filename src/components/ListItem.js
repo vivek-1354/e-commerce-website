f4866838d1a8ba433e1d7d746f9d8e2c71fa3897
vivek-1354 <108834876+vivek-1354@users.noreply.github.com>
2024-04-19 15:46:05 +0530

    pass data dynamically to the listItem component.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,24 +1,44 @@
 import React from 'react'
 
 const ListItem = () => {
+
+    const data = [{
+        discountedPrice: 350,
+        price: 450,
+        title: "Coke",
+        thumbnail: "pexels-olenkabohovyk-3819969.jpg"
+    },
+    {
+        discountedPrice: 250,
+        price: 350,
+        title: "Digital Camera Lens",
+        thumbnail: "https://images.pexels.com/photos/3602258/pexels-photo-3602258.jpeg?auto=compress&cs=tinysrgb&w=600"
+    }]
+
     return (
-        <div className={'item-card'}>
-            <img className={"img-fluid"} src="pexels-olenkabohovyk-3819969.jpg" alt="" />
-            <div className={"item-card__information"} >
-                <div className={"pricing"}>
-                    <span>Rs.350</span>
-                    <small>
-                        <strike>450</strike>
-                    </small>
-                </div>
-                <div className={"title"}>
-                    <h3>Coke</h3>
-                </div>
-                <button className={"cart-add"}>
-                    <span>Add to Cart</span>
-                    <img src="add-to-cart-svgrepo-com.svg" alt="" width="20px" />
-                </button>
-            </div>
+        <div className={"product-list--wrapper"}>
+            {data.map(item => {
+                return (
+                    <div className={'item-card'}>
+                        <img className={"img-fluid"} src={item.thumbnail} alt="" />
+                        <div className={"item-card__information"} >
+                            <div className={"pricing"}>
+                                <span>Rs.{item.discountedPrice}</span>
+                                <small>
+                                    <strike>{item.price}</strike>
+                                </small>
+                            </div>
+                            <div className={"title"}>
+                                <h3>{item.title}</h3>
+                            </div>
+                            <button className={"cart-add"}>
+                                <span>Add to Cart</span>
+                                <img src="add-to-cart-svgrepo-com.svg" alt="" width="20px" />
+                            </button>
+                        </div>
+                    </div>
+                )
+            })}
         </div>
     )
 }
