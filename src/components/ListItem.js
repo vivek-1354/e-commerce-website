9d87147e227da2b7732771e954fe4a3b4a59bf7d
vivek-1354 <108834876+vivek-1354@users.noreply.github.com>
2024-04-23 02:26:13 +0530

    added Loader component

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import SingleItem from './SingleItem'
 import axios from 'axios'
+import Loader from './Layout/Loader'
 
 
 
 
 const ListItem = () => {
     const [products, setProducts] = React.useState([])
+    const [isLoading, setIsLoading] = React.useState(false)
 
     React.useEffect(() => {
         // async function getData() {
@@ -15,13 +17,19 @@ const ListItem = () => {
         //     setProducts(data.items)
         // }
         // getData()
-
+        setIsLoading(true)
         axios.get("https://e-commerce-9691-default-rtdb.firebaseio.com/.json")
-            .then(response => setProducts(response.data.items))
+            .then(response => {
+                setTimeout(() => {
+                    setProducts(response.data.items)
+                    setIsLoading(false)
+                }, 5000)
+            })
             .catch(err => console.log(err))
-    })
+    }, [])
     return (
         <div className={"product-list"}>
+            {isLoading && <Loader />}
             <div className={"product-list--wrapper"}>
                 {products.length > 0 && products.map(item => <SingleItem item={item} />)}
             </div>
